5a5027531a41a84ebc7d10ab8232b0cf85d64aa1
yohl <yolee1003@gmail.com>
2018-11-07 20:00:20 -0500

    Made the Sign Up Page

--- src/Components/SignUpPage/SignUp.js
+++ src/Components/SignUpPage/SignUp.js
@@ -17,30 +17,29 @@ class SignUp extends Component {
 
                                     <InputPage/>
 
-                                    <div>
-                                                <select className="browser-default custom-select">
-                                                <option>Choose your option</option>
-                                                <option value="1">KSU</option>
-                                                <option value="2">Option 2</option>
-                                                <option value="3">Option 3</option>
-                                                </select>
-                                    </div>
-
-                                    <Input label="Email" icon="envelope-o" group type="email" validate error="wrong" success="right"/>
-                                    <Input label="Password" icon="lock" group type="password" validate/>
-                                    <Input label="First Name" icon="bug" id="inputLGEx"/>
-                                    <Input label="Last Name" icon="bug" id="inputLGEx"/>
-
+                                    <Input label="Email" group type="email" validate error="wrong" success="right"/>
+                                    <Input label="Password" group type="password" validate/>
+                                    <Input label="First Name" />
+                                    <Input label="Last Name" />
                                 </div>
+
                                 <div className="text-center py-4 mt-3">
-                                    <Button color="yellow darken-2" type="submit">Done! (clapping intensifies) </Button>
+                                    <Button color="yellow darken-2" type="submit">Done!</Button>
                                 </div>
+
                             </form>
                         </CardBody>
                     </Card>
-                    <script src="jquery/jquery.js"></script>
+
+
+
+
+
+                    
+                <script src="jquery/jquery.js"></script>
                 <script type="text/javascript" src='js/bootstrap.min.js'></script>
                 <link rel="stylesheet" href="css/bootstrap.css" />
+            
                 </div>
 
         );
